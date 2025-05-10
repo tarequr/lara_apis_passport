4e9f675e1669ef89a83b55d489ae461d93e2a627
tarequr <tarequrr8@gmail.com>
2025-05-10 23:42:33 +0600

    feat: create a auth controller

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        //
+    }
+
+    public function profile(Request $request)
+    {
+        //
+    }
+
+    public function refreshToken(Request $request)
+    {
+        //
+    }
+
+    public function logout(Request $request)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('profile', [AuthController::class, 'profile']);
+//     Route::post('logout', [AuthController::class, 'logout']);
+// });
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('profile', [AuthController::class, 'profile']);
+    Route::get('refresh', [AuthController::class, 'refreshToken']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
